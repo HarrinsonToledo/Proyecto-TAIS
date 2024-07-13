471d8e2820727672525f5123007965ac2bed765a
Harrinson Toledo Gonzalez <75257608+HarrinsonToledo@users.noreply.github.com>
2024-07-13 16:03:58 -0500

    datos

--- Frontend/sprint-generate/src/App.jsx
+++ Frontend/sprint-generate/src/App.jsx
@@ -3,11 +3,24 @@ import XMLValidator from './components/XMLValidator';
 import './App.css'
 
 function App() {
+  const [springSelect, setSpringSelect] = useState('3.3.2 (SNAPSHOT)');
+  const [javaSelect, setJavaSelect] = useState('Java 22');
+
   const [grupo, setGrupo] = useState("com.example")
   const [artefacto, setArtefacto] = useState("demo")
   const [nombre, setNombre] = useState("demo")
   const [descripcion, setDescripcion] = useState("Proyecto Demo para Sprint Boot")
   const [paquete, setPaquete] = useState(grupo + "." + artefacto)
+
+  const [jsonData, setJsonData] = useState({ key1: 'value1', key2: 'value2' });
+
+  const changeSpringSelect = (event) => {
+    setSpringSelect(event.target.value);
+  };
+
+  const changeJavaSelect = (event) => {
+    setJavaSelect(event.target.value);
+  };
 
   const InputGrupoChange = (event) => {
     setGrupo(event.target.value)
@@ -31,6 +44,28 @@ function App() {
     setPaquete(event.target.value)
   }
 
+  const onFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const onFileUpload = () => {
+    const formData = new FormData();
+    formData.append('file', selectedFile);
+    formData.append('jsonData', JSON.stringify(jsonData));
+
+    fetch('URL_DE_TU_BACKEND', {
+      method: 'POST',
+      body: formData,
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Respuesta del servidor:', data);
+      })
+      .catch(error => {
+        console.error('Hubo un error al subir el archivo:', error);
+      });
+  };
+
   return (
     <div className='container'>
       <p className='titleP colortxt'><b>Generador de Api's de Spring Boot</b></p>
@@ -42,35 +77,84 @@ function App() {
       <div className='options borde-b-w'>
         <p className='titleS'><b>Opciones Generales</b></p>
         <section className='sect'>
-          <label className='c-radio'>3.3.2 (SNAPSHOT)
-            <input name='v-spring' type="radio" />
-            <span class="checkmark"></span>
+          <label className='c-radio'>
+            3.3.2 (SNAPSHOT)
+            <input
+              name='v-spring'
+              type="radio"
+              value="3.3.2 (SNAPSHOT)"
+              checked={springSelect === "3.3.2 (SNAPSHOT)"}
+              onChange={changeSpringSelect}
+            />
+            <span className="checkmark"></span>
           </label>
-          <label className='c-radio'>3.3.1
-            <input name='v-spring' type="radio" />
-            <span class="checkmark"></span>
+          <label className='c-radio'>
+            3.3.1
+            <input
+              name='v-spring'
+              type="radio"
+              value="3.3.1"
+              checked={springSelect === "3.3.1"}
+              onChange={changeSpringSelect}
+            />
+            <span className="checkmark"></span>
           </label>
-          <label className='c-radio'>3.2.8 (SNAPSHOT)
-            <input name='v-spring' type="radio" />
-            <span class="checkmark"></span>
+          <label className='c-radio'>
+            3.2.8 (SNAPSHOT)
+            <input
+              name='v-spring'
+              type="radio"
+              value="3.2.8 (SNAPSHOT)"
+              checked={springSelect === "3.2.8 (SNAPSHOT)"}
+              onChange={changeSpringSelect}
+            />
+            <span className="checkmark"></span>
           </label>
-          <label className='c-radio'>3.2.7
-            <input name='v-spring' type="radio" />
-            <span class="checkmark"></span>
+          <label className='c-radio'>
+            3.2.7
+            <input
+              name='v-spring'
+              type="radio"
+              value="3.2.7"
+              checked={springSelect === "3.2.7"}
+              onChange={changeSpringSelect}
+            />
+            <span className="checkmark"></span>
           </label>
         </section>
         <section className='sect'>
-          <label className='c-radio'>Java 22
-            <input name='v-java' type="radio" />
-            <span class="checkmark"></span>
+          <label className='c-radio'>
+            Java 22
+            <input
+              name='v-java'
+              type="radio"
+              value="Java 22"
+              checked={javaSelect === "Java 22"}
+              onChange={changeJavaSelect}
+            />
+            <span className="checkmark"></span>
           </label>
-          <label className='c-radio'>Java 21
-            <input name='v-java' type="radio" />
-            <span class="checkmark"></span>
+          <label className='c-radio'>
+            Java 21
+            <input
+              name='v-java'
+              type="radio"
+              value="Java 21"
+              checked={javaSelect === "Java 21"}
+              onChange={changeJavaSelect}
+            />
+            <span className="checkmark"></span>
           </label>
-          <label className='c-radio'>Java 17
-            <input name='v-java' type="radio" />
-            <span class="checkmark"></span>
+          <label className='c-radio'>
+            Java 17
+            <input
+              name='v-java'
+              type="radio"
+              value="Java 17"
+              checked={javaSelect === "Java 17"}
+              onChange={changeJavaSelect}
+            />
+            <span className="checkmark"></span>
           </label>
         </section>
       </div>
@@ -99,6 +183,10 @@ function App() {
       </div>
       <div className='xml-data'>
         <p className='titleS'><b>Metamodelo XML</b></p>
+        <input type="file" onChange={onFileChange} />
+        <button onClick={onFileUpload}>
+          Subir Archivo
+        </button>
       </div>
     </div>
   )

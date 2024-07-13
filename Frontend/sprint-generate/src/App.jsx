004f2975cb3f812c2c009d8f3758710b6c97bdf9
Harrinson Toledo Gonzalez <75257608+HarrinsonToledo@users.noreply.github.com>
2024-07-13 14:22:39 -0500

    try xml fail

--- Frontend/sprint-generate/src/App.jsx
+++ Frontend/sprint-generate/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import XMLValidator from './components/XMLValidator';
 import './App.css'
 
 function App() {
@@ -42,59 +43,62 @@ function App() {
         <p className='titleS'><b>Opciones Generales</b></p>
         <section className='sect'>
           <label className='c-radio'>3.3.2 (SNAPSHOT)
-            <input name='v-spring' type="radio"/>
-              <span class="checkmark"></span>
+            <input name='v-spring' type="radio" />
+            <span class="checkmark"></span>
           </label>
           <label className='c-radio'>3.3.1
-            <input name='v-spring' type="radio"/>
-              <span class="checkmark"></span>
+            <input name='v-spring' type="radio" />
+            <span class="checkmark"></span>
           </label>
           <label className='c-radio'>3.2.8 (SNAPSHOT)
-            <input name='v-spring' type="radio"/>
-              <span class="checkmark"></span>
+            <input name='v-spring' type="radio" />
+            <span class="checkmark"></span>
           </label>
           <label className='c-radio'>3.2.7
-            <input name='v-spring' type="radio"/>
-              <span class="checkmark"></span>
+            <input name='v-spring' type="radio" />
+            <span class="checkmark"></span>
           </label>
         </section>
         <section className='sect'>
-        <label className='c-radio'>Java 22
-            <input name='v-java' type="radio"/>
-              <span class="checkmark"></span>
+          <label className='c-radio'>Java 22
+            <input name='v-java' type="radio" />
+            <span class="checkmark"></span>
           </label>
           <label className='c-radio'>Java 21
-            <input name='v-java' type="radio"/>
-              <span class="checkmark"></span>
+            <input name='v-java' type="radio" />
+            <span class="checkmark"></span>
           </label>
           <label className='c-radio'>Java 17
-            <input name='v-java' type="radio"/>
-              <span class="checkmark"></span>
+            <input name='v-java' type="radio" />
+            <span class="checkmark"></span>
           </label>
         </section>
       </div>
-      <div className='form-data'>
-      <p className='titleS'><b>Metadata Proyecto</b></p>
+      <div className='form-data borde-b-w'>
+        <p className='titleS'><b>Metadata Proyecto</b></p>
         <section className='individual'>
           <p className='in-label'>Grupo</p>
-          <input type="text" value={grupo} onChange={InputGrupoChange} className='in-txt'/>
+          <input type="text" value={grupo} onChange={InputGrupoChange} className='in-txt' />
         </section>
         <section className='individual'>
           <p className='in-label'>Artefacto</p>
-          <input type="text" value={artefacto} onChange={InputArtefactoChange} className='in-txt'/>
+          <input type="text" value={artefacto} onChange={InputArtefactoChange} className='in-txt' />
         </section>
         <section className='individual'>
           <p className='in-label'>Nombre</p>
-          <input type="text" value={nombre} onChange={InputNombreChange} className='in-txt'/>
+          <input type="text" value={nombre} onChange={InputNombreChange} className='in-txt' />
         </section>
         <section className='individual'>
           <p className='in-label'>Descripción</p>
-          <input type="text" value={descripcion} onChange={InputDescripcionChange} className='in-txt'/>
+          <input type="text" value={descripcion} onChange={InputDescripcionChange} className='in-txt' />
         </section>
         <section className='individual'>
           <p className='in-label'>Nombre Paquete</p>
-          <input type="text" value={paquete} onChange={InputPaqueteChange} className='in-txt'/>
+          <input type="text" value={paquete} onChange={InputPaqueteChange} className='in-txt' />
         </section>
+      </div>
+      <div className='xml-data'>
+        <p className='titleS'><b>Metamodelo XML</b></p>
       </div>
     </div>
   )

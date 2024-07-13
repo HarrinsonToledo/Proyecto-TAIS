ce2122727402c22ceb2cc4d42b4ee18f26b110a8
JuanEstebanVanegas <juanesvanegas1102@gmail.com>
2024-07-13 15:58:00 -0500

    Endpoint

---
+++ Backend/src/main/java/backend/demo/Logica/Estructura.java
@@ -0,0 +1,17 @@
+package backend.demo.Logica;
+
+public class Estructura {
+    public String primerNombre;
+    public String segundoNombre;
+    public String primerApellido;
+    public String segundoApellido;
+    public String direccion;
+    public String correo;
+    public int codRepresentante;
+    public String telefono;
+    public int identificacion;
+    public String tipoDocumento;
+    public String genero;
+    public String ciudad;
+    public String fechaNacimiento;
+}

0088ca0f78010fae5837407334ca284bdc39bf7c
JuanEstebanVanegas <juanesvanegas1102@gmail.com>
2024-07-15 14:46:45 -0500

    Manejo errores validacion XML

--- Backend/src/main/java/backend/demo/Controllers/XMLController.java
+++ Backend/src/main/java/backend/demo/Controllers/XMLController.java
@@ -10,6 +10,7 @@ import javax.xml.validation.Schema;
 import javax.xml.validation.SchemaFactory;
 import javax.xml.validation.Validator;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,26 +20,33 @@ import org.springframework.web.multipart.MultipartFile;
 import org.xml.sax.SAXException;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/xml")
 public class XMLController {
 
     @PostMapping("/validar")
-    String validar(@RequestParam("xml") MultipartFile xml, @RequestPart("jsonData") String jsonData) throws IOException, SAXException {
+    String validar(@RequestParam("xml") MultipartFile xml, @RequestPart("jsonData") String jsonData)
+            throws IOException, SAXException {
 
-        Source source = new StreamSource(xml.getInputStream());
-        File xsdFile = new File("Backend\\src\\main\\java\\backend\\demo\\Controllers\\entidadesProyecto.xsd");
-        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        try {
+            Source source = new StreamSource(xml.getInputStream());
+            File xsdFile = new File("Backend\\src\\main\\java\\backend\\demo\\Controllers\\entidadesProyecto.xsd");
+            SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
 
-        Schema schema = schemaFactory.newSchema(xsdFile);
-        Validator validator = schema.newValidator();
-        validator.validate(source);
-        return "XML valido";
+            Schema schema = schemaFactory.newSchema(xsdFile);
+            Validator validator = schema.newValidator();
+            validator.validate(source);
+
+            return "XML valido";
+        } catch (SAXException e) {
+            return "Error de validación XML: " + e.getMessage();
+        }
 
         /*
          * String xmlContent = new BufferedReader(
-            new InputStreamReader(xmlFile.getInputStream(), StandardCharsets.UTF_8))
-            .lines()
-            .collect(Collectors.joining("\n"));
+         * new InputStreamReader(xmlFile.getInputStream(), StandardCharsets.UTF_8))
+         * .lines()
+         * .collect(Collectors.joining("\n"));
          */
     }
 }
